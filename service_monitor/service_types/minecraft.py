03654adc7b3219760fbd6e1522c6443dd7a35035
necavi <necavi@0xf.org>
2016-04-18 21:20:02 -0700

    Added minecraft colourization

--- service_monitor/service_types/minecraft.py
+++ service_monitor/service_types/minecraft.py
@@ -2,6 +2,47 @@ from mcstatus import MinecraftServer
 
 from service_monitor.service_base import ServiceBase
 
+colours = {
+    "0": "000000",
+    "1": "0000AA",
+    "2": "00AA00",
+    "3": "00AAAA",
+    "4": "AA0000",
+    "5": "AA00AA",
+    "6": "FFAA00",
+    "7": "AAAAAA",
+    "8": "555555",
+    "9": "5555FF",
+    "a": "55FF55",
+    "b": "55FFFF",
+    "c": "FF5555",
+    "d": "FF55FF",
+    "e": "FFFF55",
+    "f": "FFFFFF"
+}
+
+
+def colourize(buff):
+    reading_colour = False
+    in_colour = False
+    out = ""
+    for char in buff:
+        if char == "§":
+            if in_colour:
+                out += "</span>"
+                in_colour = False
+            reading_colour = True
+        elif reading_colour:
+            if char in colours.keys():
+                out += "<span style='color: #{}'>".format(colours[char])
+            else:
+                reading_colour = False
+        else:
+            out += char
+    if in_colour:
+        out += "</span>"
+    return out
+
 
 class MinecraftService(ServiceBase):
     type = "Minecraft"
@@ -24,6 +65,6 @@ class MinecraftService(ServiceBase):
         details.update({
             "Max Players": info.players.max,
             "Online Players": info.players.online,
-            "Description": info.description
+            "Description": colourize(info.description)
         })
         return details
